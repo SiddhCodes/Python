6f9456306492b17f43f8650b45868045ac81e440
Siddhant_Mul <siddhantmul18@gmail.com>
2025-11-30 21:52:12 +0530

    Add initial content for input/output and operators sections with examples and explanations

---
+++ Input and output/main.py
@@ -0,0 +1,12 @@
+'''Output'''
+# You probably know till now, how to provide the output of the code you have written and that is with print() function.
+# There is no other functions to provide the result on the terminal we just have to use print() function
+# Now when providing the output we can use variables in print statement using a formatted string as shown in the below example
+name = input("Enter your name: ")
+print(f"Hello {name} ! Welcome to Python programing.")
+
+'''Input'''
+# But the main question is how to ask user for some information.
+# For example there is a user and you want to ask the age of that user, how can you do so, it’s easy using input().
+# Now the default data type of input is always string reason is simple you can store anything in string.
+# You have to manually convert the data type of input statements.
---
+++ Operators/main.py
@@ -0,0 +1,67 @@
+'''What are operators'''
+#  Operators are symbols that perform operations on variables and values. Python has  types of operators for different tasks like arithmetic, comparison, logical operations, and more
+# Lets see every operators one by one
+
+'''Arithmetic operators'''
+# Arithmetic operators perform mathematical operations like addition, subtraction, multiplication, division, etc
+
+# There are 7 types of arithmetic operator.
+# addition         +                      
+# subtraction      -                    
+# multiplication   *                    
+# division         /                      
+# Floor division   //                    
+# modulus          %
+# Exponentiation   **
+
+# Eg - a = 12 
+# b = 8
+# print(a+b)
+# Output - 20
+
+'''Assignment operator'''
+# Assignment operators are used to assign values to variables. Python also provides compound assignment operators that perform operations like addition, subtraction, multiplication, etc
+# A basic assignment operator is simple =.
+
+'''Compound assignment operator'''
+# Compound assignment operator combines arithmetic operations with assignment
+# But first you have to understand how things work when we reassign variables in python and also reassigning variables with addition, subtraction etc.
+# Using compound assignment operators the reassigning works better
+
+# +=      Add and assig
+# -=      Subtract and assig                                        
+# *=      Multiply and assign                                        
+# /=      Divide and assig                                        
+# //=     Floor divide and assig                                      
+# %=      modulus and assig                                      
+# **=     Exponentiation and assign                                        
+
+'''Comparison operator'''
+# Comparison operators, also called relational operators, are used to compare two values
+# Comparison operators will always provide Boolean result that is True and False
+# omparison operators are as follows
+
+# ==    Equal to                                     
+# !=    Not Equal to                                      
+# >     Greater than                                      
+# <     Less than                                      
+# >=    Greater than or equal to                                     
+# <=    Less than or equal to
+
+#  Comparison operators will work with numbers but you can use them with strings as well.
+# Strings will be comparing the Ascii values of string.                                  
+# 
+
+'''Logical operators'''
+# Logical operators in Python are used to combine multiple conditions and return a Boolean result (True or False)
+# There are 3 types of logical operator
+# and   - Return True if both condition are True
+# or      - Return True if at least one condition is True
+# not    - Reverse the boolean value
+
+'''Trivial Questions'''
+# Answer True and Fals
+# Print(126 > 130
+# print((456 == 456) != (235 == 236)
+# print(12 < 10 or 45 == 56 or 69 > 70 or 15 != 13
+# print(True and bool(0))
